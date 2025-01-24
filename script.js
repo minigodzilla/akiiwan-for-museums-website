7388e163a7c67ddcb096385c5c1907560fc0294c
Steve Diabo <stevediabo@gmail.com>
2025-01-24 14:15:26 -0500

    FormPee

--- script.js
+++ script.js
@@ -85,4 +85,39 @@ document.addEventListener('DOMContentLoaded', () => {
     // window.addEventListener('hashchange', slideToAnchor);
 
     document.querySelector('#btn-start').addEventListener('click', () => swiper.slideNext());
+
+    // feedback form
+
+    const form = document.getElementById('feedback-form');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        var status = document.getElementById('status-message');
+        var data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                Accept: 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    status.innerHTML = 'Thanks for your submission!';
+                    form.reset();
+                } else {
+                    response.json().then((data) => {
+                        if (Object.hasOwn(data, 'errors')) {
+                            status.innerHTML = data['errors'].map((error) => error['message']).join(', ');
+                        } else {
+                            status.innerHTML = 'Oops! There was a problem submitting your form';
+                        }
+                    });
+                }
+            })
+            .catch((error) => {
+                status.innerHTML = 'Oops! There was a problem submitting your form';
+            });
+    }
+    form.addEventListener('submit', handleSubmit);
 });

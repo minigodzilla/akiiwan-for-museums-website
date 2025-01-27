aab04650d8050043c0329adc833003e074bd202c
Steve Diabo <stevediabo@gmail.com>
2025-01-27 13:39:17 -0500

    mailChimp form stuff WIP

--- script.js
+++ script.js
@@ -89,11 +89,39 @@ document.addEventListener('DOMContentLoaded', () => {
     // feedback form
 
     const form = document.getElementById('feedback-form');
+    const status = document.getElementById('status-message');
 
     async function handleSubmit(event) {
         event.preventDefault();
-        var status = document.getElementById('status-message');
-        var data = new FormData(event.target);
+
+        const allInputs = form.querySelectorAll('input, textarea, button');
+        const hpValue = form.querySelector('#b_e7f83e702318920efe670e9b1_597f4b8abc').value;
+        const nameValue = form.querySelector('#name').value;
+        const emailValue = form.querySelector('#email').value;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const mailchimpConsent = form.querySelector('#mailchimp-consent').checked;
+        const data = new FormData(event.target);
+
+        // Jeremy: xnnjnjza | Steve: mgvovvgd
+        const formSpreeAction = 'https://formspree.io/f/mgvovvgd';
+        const mailChimpAction = 'https://gmail.us21.list-manage.com/subscribe/post-json?u=e7f83e702318920efe670e9b1&amp;id=597f4b8abc&amp;f_id=0056f5e6f0';
+
+        console.log(event.target.action);
+
+        if (!nameValue) {
+            status.innerHTML = 'Please enter your name';
+            return;
+        }
+
+        if (!emailRegex.test(emailValue)) {
+            status.innerHTML = 'Please enter a valid email address';
+            return;
+        }
+
+        // disable the form while we wait for the response
+        allInputs.forEach((el) => (el.disabled = true));
+
+        // FormSpree submission
         fetch(event.target.action, {
             method: form.method,
             body: data,
@@ -103,21 +131,69 @@ document.addEventListener('DOMContentLoaded', () => {
         })
             .then((response) => {
                 if (response.ok) {
-                    status.innerHTML = 'Thanks for your submission!';
-                    form.reset();
+                    // status.innerHTML = 'Thanks for your submission!';
+                    // form.reset();
+                    console.log('FormSpree response:', response);
                 } else {
                     response.json().then((data) => {
-                        if (Object.hasOwn(data, 'errors')) {
-                            status.innerHTML = data['errors'].map((error) => error['message']).join(', ');
-                        } else {
-                            status.innerHTML = 'Oops! There was a problem submitting your form';
-                        }
+                        status.innerHTML = 'Oops! There was a problem submitting your form';
+                        allInputs.forEach((el) => (el.disabled = false));
+                        console.log('FormSpree error:', data);
+                        return;
                     });
                 }
             })
             .catch((error) => {
                 status.innerHTML = 'Oops! There was a problem submitting your form';
+                allInputs.forEach((el) => (el.disabled = false));
             });
+
+        // MailChimp submission
+        if (mailchimpConsent) {
+            mailChimpAction += '&EMAIL=' + encodeURIComponent(emailValue) + '&group[2198]=32&b_e7f83e702318920efe670e9b1_597f4b8abc=' + encodeURIComponent(hpValue) + '&c=displayMailChimpStatus';
+
+            // Inject a script with the output from the post action
+            // i.e. If all goes well, the post action should output "displayMailChimpStatus({"result":"success","msg":"Thank you for subscribing!"})"
+            const script = document.createElement('script');
+            script.src = mailChimpAction;
+
+            // insert script tag into the DOM
+            const ref = window.document.getElementsByTagName('script')[0];
+            ref.parentNode.insertBefore(script, ref);
+
+            console.log('MailChimp action:', mailChimpAction);
+
+            // After the script is loaded (and executed), remove it
+            script.onload = () => {
+                script.remove();
+            };
+        }
     }
+
+    // Process the form action output arguments from MailChimp
+    window.displayMailChimpStatus = (response) => {
+        // Make sure the data is in the right format
+        if (!response.result || !response.msg) return;
+
+        // Check if the message contains "thank you" (case insensitive)
+        const thankYouRegex = /thank you/i;
+        const containsThankYou = thankYouRegex.test(response.msg);
+
+        // Choose an appropriate action based on the presence of "thank you"
+        if (containsThankYou) {
+            // Display a custom status message
+            status.innerHTML = 'To complete the subscription process, please click the confirmation link in the email we just sent.';
+        } else {
+            // Output the raw endpoint status message
+            status.innerHTML = response.msg;
+        }
+
+        // If successful, set the appropriate class to the form;
+        // otherwise, re-enable all form inputs and reset the form class
+        if (response.result === 'success') {
+        } else {
+            allInputs.forEach((input) => (input.disabled = false));
+        }
+    };
     form.addEventListener('submit', handleSubmit);
 });
